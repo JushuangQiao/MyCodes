54502fbcd55db57e312caee917bbf4fdd0db5c7d
qiao <747848783@qq.com>
2016-11-18 19:30:19 +0800

    commit leetcode 2

--- leetcode/2.py
+++ leetcode/2.py
@@ -7,33 +7,31 @@
 #         self.next = None
 
 
-class Solution(object):
+class Solution(object): 
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        ret = []
+        head = move = ListNode(0)
         temp = 0
         while l1 and l2:
-            val = (l1.val + l2.val + temp) % 10
+            move.next = ListNode((l1.val + l2.val + temp) % 10)
             temp = (l1.val + l2.val + temp) / 10
-            ret.append(val)
-            print ret
             l1 = l1.next
             l2 = l2.next
+            move = move.next
         while l1:
-            val = (l1.val + temp) % 10
+            move.next = ListNode((l1.val + temp) % 10)
             temp = (l1.val + temp) / 10
-            ret.append(val)
-            print ret
             l1 = l1.next
+            move = move.next
         while l2:
-            val = (l2.val + temp) % 10
+            move.next = ListNode((l2.val + temp) % 10)
             temp = (l2.val + temp) / 10
-            ret.append(val)
             l2 = l2.next
+            move = move.next
         if temp != 0:
-            ret.append(temp)
-        return ret
+            move.next = ListNode(temp)
+        return head.next
