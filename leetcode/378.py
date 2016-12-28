e27661f304b74ea345f25ed3dc25f5aabf6b781a
qiao <747848783@qq.com>
2016-12-28 17:54:22 +0800

    add 378

---
+++ leetcode/378.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        return sorted([j for i in matrix for j in i])[k-1]
+
