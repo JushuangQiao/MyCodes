e551fa287bb3063a7a2d1215ef3cbfbb90f43265
qiao <747848783@qq.com>
2017-04-14 20:15:47 +0800

    add 77

---
+++ leetcode/77.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        from itertools import combinations
+        return list(combinations([i for i in range(1, n+1)], k))
