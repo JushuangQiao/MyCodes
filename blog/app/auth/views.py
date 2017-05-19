161f12ce4ef667b32c53da3a13abfac15b822d35
qiao <747848783@qq.com>
2017-05-18 19:17:17 -0700

    add 34

--- blog/app/auth/views.py
+++ blog/app/auth/views.py
@@ -2,9 +2,8 @@
 
 import logging
 from flask import render_template, redirect, url_for, request, flash
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import logout_user, login_required, current_user
 from . import auth
-from ..models.models import User
 from ..models.manager import UserManager
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm
 from .. import db
@@ -21,14 +20,10 @@ def login():
     form = LoginForm()
     try:
         if form.validate_on_submit():
-            user = User.query.filter_by(email=form.email.data).first()
-            # next line why verify is false?
-            # if user is not None and user.verify_password(form.password.data):
-            # 确认密码这里，为什么总是不行呢？
-            if user is not None:
-                login_user(user, form.remember_me.data)
+            user = UserManager.verify_password(form)
+            if user:
                 return redirect(request.args.get('next') or url_for('main.home'))
-                # flash(u'用户名或密码错误')
+            flash(u'用户名或密码错误')
         return render_template('auth/login.html', form=form)
     except Exception, e:
         logging.error('func: login error:{0}'.format(e))
@@ -53,7 +48,7 @@ def register():
             return redirect(url_for('auth.login'))
         return render_template('auth/register.html', form=form)
     except Exception, e:
-        logging.error('func: register error:{0}'.format(e))
+        logging.error('func: register info:{0}'.format(e))
         return render_template('auth/register.html', form=None)
 
 
@@ -62,7 +57,6 @@ def change_password():
     form = ChangePasswordForm()
     try:
         if form.validate_on_submit():
-            # 同样的问题，验证密码不成功，后续解决!
             if current_user.verify_password(form.old_password.data):
                 current_user.password = form.password.data
                 db.session.add(current_user)
