96b2bf1f738523272cbc09a39e5e1ba7bd1fe0cb
qiaojushuang <747848783@qq.com>
2017-10-16 12:25:59 +0800

    trees

---
+++ leetcode/test_codes/codes/trees.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Tree(object):
+    pass
