d6e48571805315197aa016808c09c9870a3ab343
qiaojushuang <747848783@qq.com>
2017-10-19 21:46:05 +0800

    350

---
+++ leetcode/python/350.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        tmp_dict = dict()
+        ret = []
+        for i in nums1:
+            tmp_dict[i] = tmp_dict[i] + 1 if tmp_dict.get(i) else 1
+        for n in nums2:
+            if n in tmp_dict:
+                ret.append(n)
+                tmp_dict[n] -= 1
+                if tmp_dict[n] == 0:
+                    tmp_dict.pop(n)
+        return ret
+
