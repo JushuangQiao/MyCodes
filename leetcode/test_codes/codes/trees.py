3133693f5ce5f8db854a359857898ded3ce01610
qiaojushuang <747848783@qq.com>
2017-10-20 20:39:51 +0800

    trees

--- leetcode/test_codes/codes/trees.py
+++ leetcode/test_codes/codes/trees.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from collections import deque
+
 
 class TreeNode(object):
     def __init__(self, x):
@@ -9,4 +11,53 @@ class TreeNode(object):
 
 
 class Tree(object):
-    pass
+    def __init__(self):
+        self.root = None
+    
+    def construct_tree(self, values=None):
+        if not values:
+            return None
+        self.root = TreeNode(values[0])
+        queue = deque([self.root])
+        leng = len(values)
+        nums = 1
+        while nums < leng:
+            node = queue.popleft()
+            if node:
+                node.left = TreeNode(values[nums]) if values[nums] else None
+                queue.append(node.left)
+                if nums + 1 < leng:
+                    node.right = TreeNode(values[nums+1]) if values[nums+1] else None
+                    queue.append(node.right)
+                    nums += 1
+                nums += 1
+    
+    def bfs(self):
+        ret = []
+        queue = deque([self.root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                ret.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+        return ret
+
+    def pre_traversal(self):
+        ret = []
+
+        def traversal(head):
+            if not head:
+                return
+            ret.append(head.val)
+            traversal(head.left)
+            traversal(head.right)
+        traversal(self.root)
+        return ret
+        
+
+t = Tree()
+t.construct_tree([1,2,None,4,None])
+print t.bfs()
+print t.pre_traversal()
+
