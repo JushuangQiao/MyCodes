4975a66e73d651f3d5ac62831539c20e7b25c4bf
qiaojushuang <747848783@qq.com>
2017-11-21 07:30:27 -0800

    test

--- leetcode/test_codes/solution.py
+++ leetcode/test_codes/solution.py
@@ -7,9 +7,11 @@ class Solution(object):
     def result(self):
         pass
 
+
 if __name__ == '__main__':
     node = Nodes([7, 2, 4, 3])
     node.print_self()
     link = node.get_link()
     # Nodes.print_link(link)
     # Solution().result()
+    # test
